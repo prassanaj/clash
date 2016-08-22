34385094abe44e81cd1a578912d249cb0bd1b4a0
janarthp <janarthp@deshaw.com>
2016-08-22 15:14:24 +0530

    some changes to include the team total and few other stats

--- model.py
+++ model.py
@@ -37,15 +37,19 @@ from tabulate import tabulate
 from optparse import OptionParser
 
 
-def team_matrix(player_df, team):
+def team_matrix(player_df, details, team):
     players = player_df.loc[:, ['Team', 'Login', 'Gender', 'Game', 'Ranks', 'Price', 'Sold For']].drop_duplicates()
-    players['Login - R'] = players.Login.str.cat(players.Ranks.astype(str), sep='-').str.cat(players.Gender, sep='-')
+    players['Login - R'] = players.Login.str.cat(players.Ranks.astype(str), sep=' ').str.cat(players.Gender, sep='-')
 
     clash_teams = team if (type(team) is list) else [team]
 
+    col_name = ['Spent', 'Remaining', 'Male', 'Female', 'Total']
     for team in clash_teams:
-        print team.upper()
-        print '~~~~~~~~'
+        print team.upper() + " -",
+        for d in details[team]:
+            print col_name[details[team].index(d)] + ":" + d.astype(str) + "  ",
+        print
+        print '~~~~~~~~' * 10
         t = players[players.Team == team]
         player_details = {}
         for s in Sports:
@@ -82,6 +86,10 @@ def load_excel_into_pandas(file, sheet):
     sdf = df.sort_values(by=['Team', 'Game', 'Ranks'])
     return sdf;
 
+def get_team_details(file, sheet):
+    df = pandas.read_excel(file, sheet)
+    return df.set_index('Team').T.to_dict('list')
+
 
 parser = OptionParser()
 
@@ -96,11 +104,12 @@ file = 'Clash.xlsx'
 sheet = 'Sport n Player List'
 
 Sports = ['Athletics 4+2', 'Badminton 3+1', 'Basketball 4', 'Carrom 2+1', 'Chess 4+1', 'Cricket 5', 'Foosball 5+2',
-          'Football 5', 'Snooker 4+1', 'Squash 3+1', 'Swimming 3+1', 'Tables~Tennis 5+1', 'Tennis 6+1', 'Throwball 5',
+          'Football 5', 'Snooker 4+1', 'Squash 3+1', 'Swimming 3+1', 'Table~Tennis 5+1', 'Tennis 6+1', 'Throwball 5',
           'Volleyball 5+1']
 
-Teams = ['Ninja', 'Spartans', 'Samurai', 'Knights'] if (options.team is None) else options.team
+Teams = ['Knights', 'Spartans', 'Samurai', 'Ninja'] if (options.team is None) else options.team
 
+team_details = get_team_details(file, 'Summary')
 pd = load_excel_into_pandas(file, sheet)
-team_matrix(pd, Teams)
+team_matrix(pd, team_details, Teams)
 get_player_details(pd, options.player)
